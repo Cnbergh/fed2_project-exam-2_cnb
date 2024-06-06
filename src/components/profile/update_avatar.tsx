8ba611302cc5316a6a8b454cab2b148133ba4f40
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-06-06 07:57:09 +0200

    TS errors

--- src/components/profile/update_avatar.tsx
+++ src/components/profile/update_avatar.tsx
@@ -1,23 +1,31 @@
-'use client'
+'use client';
+
 import React, { useState } from 'react';
 import { useAuth } from '../providers/auth_context';
 import { useApi } from '@/api/api';
 
-const UpdateAvatar = () => {
+interface UpdateAvatarProps {
+  onClose: () => void;
+}
+
+const UpdateAvatar: React.FC<UpdateAvatarProps> = ({ onClose }) => {
   const { authState, saveUserData } = useAuth();
   const { updateProfile } = useApi();
   const [avatarUrl, setAvatarUrl] = useState('');
 
-  const onSubmit = async (e) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!authState.user) {
+      console.error('User is not logged in');
+      return;
+    }
+
     const profileData = {
       avatar: {
         url: avatarUrl,
         alt: 'avatar',
       },
     };
-    console.log('Current Auth State in UpdateAvatar:', authState);
-    console.log('Profile data to be updated:', profileData);
 
     try {
       const response = await updateProfile(authState.user.name, profileData);
@@ -26,6 +34,7 @@ const UpdateAvatar = () => {
       // Update the auth state with the new avatar
       const updatedUser = { ...authState.user, avatar: profileData.avatar };
       saveUserData({ ...authState, user: updatedUser });
+      onClose();
     } catch (error) {
       console.error('Failed to update avatar:', error);
     }
