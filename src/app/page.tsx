3c53079aa1d1e860f0d63891e0610fb4846582a0
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-06-06 08:31:45 +0200

    Scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,29 @@
 import Hero from '@/components/hero';
 import Venues from './venues/page';
 import { Suspense } from 'react';
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 
 export default function Home() {
   return (
     <div className="m-2">
       <main className="bg-slate-50 py-3 rounded-[36px] m-auto max-w-[1780px]">
         <Suspense fallback={<div>Loading...</div>}>
-          <Hero />
-          <section className="bg-slate-100 mt-1 rounded-[36px] m-auto">
-            <Venues />
-          </section>
+          <ScrollArea.Root className="w-full h-dvh rounded overflow-hidden">
+            <ScrollArea.Viewport className="w-full h-full rounded">
+              <Hero />
+              <section className="bg-slate-100 mt-1 rounded-[36px] m-auto">
+                <Venues />
+              </section>
+            </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar
+              orientation="vertical">
+              <ScrollArea.Thumb/>
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Scrollbar>
+              <ScrollArea.Thumb />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Corner />
+          </ScrollArea.Root>
         </Suspense>
       </main>
     </div>
