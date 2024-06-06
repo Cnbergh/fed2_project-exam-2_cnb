575c2b2fe3185a9c8a3292f174e49f7b6829bcf0
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-06-06 07:22:19 +0200

    TS error vercel

--- src/components/header/navbar/nav-menu.tsx
+++ src/components/header/navbar/nav-menu.tsx
@@ -24,42 +24,37 @@ export const navData = [
 import Link from "next/link";
 
 // next router
-import { useRouter } from "next/navigation";
-
+import { usePathname } from "next/navigation";
 
 //My Nav
 const NavMenu = () => {
-  const router = useRouter();
-  const pathname = router.pathname;
+  const pathname = usePathname();
   return (
     <div className="pl-1">
-    <nav className="bg-white border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-ms transition cursor-pointer">
-      {/* inner */}
-      <div
-        className="hidden md:flex flex-row items-center justify-between"
-      >
-        {navData.map((link, index) => {
-          return (
-            <Link
-              className={`${
-                link.path === pathname && "text-teal-500"
-              } relative flex items-center group hover:text-teal-500 transition-all duration-300 px-6`}
-              href={link.path}
-              key={index}
-            >
-              {/* tooltip */}
-              <div className="hidden lg:block">{link.name}</div>
+      <nav className="bg-white border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-ms transition cursor-pointer">
+        {/* inner */}
+        <div className="hidden md:flex flex-row items-center justify-between">
+          {navData.map((link, index) => {
+            return (
+              <Link
+                className={`${
+                  link.path === pathname && "text-teal-500"
+                } relative flex items-center group hover:text-teal-500 transition-all duration-300 px-6`}
+                href={link.path}
+                key={index}
+              >
+                {/* tooltip */}
+                <div className="hidden lg:block">{link.name}</div>
 
-              {/* icon */}
-              <div className="pb-1 pl-1 lg:pl-2">{link.icon}</div>
-            </Link>
-          );
-        })}
-      </div>
-    </nav>
+                {/* icon */}
+                <div className="pb-1 pl-1 lg:pl-2">{link.icon}</div>
+              </Link>
+            );
+          })}
+        </div>
+      </nav>
     </div>
   );
 };
-
 
 export default NavMenu;
