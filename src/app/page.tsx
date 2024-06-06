0f00250c3a1d4c8249efb9651a086f1cf02ff492
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2024-06-06 08:18:40 +0200

    Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 import Hero from '@/components/hero';
 import Venues from './venues/page';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="m-2">
       <main className="bg-slate-50 py-3 rounded-[36px] m-auto max-w-[1780px]">
-        <Hero />
-      <section className="bg-slate-100 mt-1 rounded-[36px] m-auto">
-        <Venues />
-      </section>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Hero />
+          <section className="bg-slate-100 mt-1 rounded-[36px] m-auto">
+            <Venues />
+          </section>
+        </Suspense>
       </main>
     </div>
   );
